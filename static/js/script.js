181904fb14a837188beb4ac150bf11e2e706c2ab
Astitva1508 <21bcs015@nith.ac.in>
2024-04-15 11:10:47 +0530

    Designing the player card

--- static/js/script.js
+++ static/js/script.js
@@ -173,14 +173,14 @@ function playerCard(player){
     <img src="/static/data/player_image.jpg" class="card-img-top player-img" alt="Image Not Found">
     <h5 class="card-title">${player.Player}</h5>
     <div class="card-body">
-<div class="accordion" id="accordionPanelsStayOpenExample">
+<div class="accordion" id="parentAccordion">
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
         General Stats
     </button>
     </h2>
-    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#parentAccordion">
       <div class="accordion-body">
         <p><strong>Position:</strong> ${player.Position}</p>
         <p><strong>Team:</strong> ${player.Team}</p>
@@ -189,11 +189,11 @@ function playerCard(player){
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
         Physical Stats
       </button>
     </h2>
-    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
       <div class="accordion-body">
         <p><strong>Age:</strong> ${player.Age}</p>
         <p><strong>Height:</strong> ${player.Length}</p>
@@ -203,27 +203,24 @@ function playerCard(player){
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
         Match Stats
       </button>
     </h2>
-    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
       <div class="accordion-body">
-        <!-- Dribble Skills -->
         <div class="skill-bar">
             <p><strong>Dribble Skills:</strong></p>
             <div class="progress">
                 <div class="progress-bar" role="progressbar" style="width: ${player['Dribble Skills']}%;" aria-valuenow="${player['Dribble Skills']}" aria-valuemin="0" aria-valuemax="100">${player['Dribble Skills']}%</div>
             </div>
         </div>
-        <!-- Ball Control -->
         <div class="skill-bar">
             <p><strong>Ball Control:</strong></p>
             <div class="progress">
                 <div class="progress-bar" role="progressbar" style="width: ${player['Ball Control']}%;" aria-valuenow="${player['Ball Control']}" aria-valuemin="0" aria-valuemax="100">${player['Ball Control']}%</div>
             </div>
         </div>
-        <!-- Passing under Pressure -->
         <div class="skill-bar">
             <p><strong>Passing under Pressure:</strong></p>
             <div class="progress">
