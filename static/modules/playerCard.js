e2e2e8c1bfde187952febee6e238f06d62654dcc
Astitva1508 <21bcs015@nith.ac.in>
2024-04-19 00:54:21 +0530

    Consistency of style

--- static/modules/playerCard.js
+++ static/modules/playerCard.js
@@ -8,74 +8,74 @@ export const renderPlayerCards = (player) => {
   playerCardContainer.innerHTML = playerCard(player);
 };
 
-
-export const playerCard = (player) =>{
-    let playerCardHtml =
-        `
-<div class="card player-card">
-    <img src="/static/data/player_image.jpg" class="card-img-top player-img" alt="Image Not Found">
+export const playerCard = (player)=>{
+  return `
+  <div class="row justify-content-between">
+    <div class="player-info col-6 mb-2">
+        <h4 class="card-title mb-2">${player.Player}</h5>
+        <p class="mb-2"><strong>Position:</strong> ${player.Position}</p>
+        <p class="mb-2"><strong>Team:</strong> ${player.Team}</p>
+    </div>
+    <div class="player-img col-6 mb-2 ">
+        <img src="/static/data/player_image.jpg" class="card-img-top" alt="Image Not Found">
+    </div>
     <div class="card-body">
-    <h5 class="card-title">${player.Player}</h5>
-<div class="accordion" id="parentAccordion">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        General Stats
-    </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
-      <div class="accordion-body">
-        <p><strong>Position:</strong> ${player.Position}</p>
-        <p><strong>Team:</strong> ${player.Team}</p>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Physical Stats
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
-      <div class="accordion-body">
-        <p><strong>Age:</strong> ${player.Age}</p>
-        <p><strong>Height:</strong> ${player.Length}</p>
-        <p><strong>Weight:</strong> ${player.Weight}</p>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Match Stats
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
-      <div class="accordion-body">
-        <div class="skill-bar">
-            <p><strong>Dribble Skills:</strong></p>
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" style="width: ${player['Dribble Skills']}%;" aria-valuenow="${player['Dribble Skills']}" aria-valuemin="0" aria-valuemax="100">${player['Dribble Skills']}%</div>
+        <div class="accordion" id="parentAccordion">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Physical Stats
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                        <p><strong>Age:</strong> ${player.Age}</p>
+                        <p><strong>Height:</strong> ${player.Length}</p>
+                        <p><strong>Weight:</strong> ${player.Weight}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Match Stats
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                        <div class="skill-bar">
+                            <p><strong>Dribble Skills:</strong></p>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar"
+                                    style="width: ${player['Dribble Skills']}%;"
+                                    aria-valuenow="${player['Dribble Skills']}" aria-valuemin="0" aria-valuemax="100">
+                                    ${player['Dribble Skills']}%</div>
+                            </div>
+                        </div>
+                        <div class="skill-bar">
+                            <p><strong>Ball Control:</strong></p>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: ${player['Ball Control']}%;"
+                                    aria-valuenow="${player['Ball Control']}" aria-valuemin="0" aria-valuemax="100">
+                                    ${player['Ball Control']}%</div>
+                            </div>
+                        </div>
+                        <div class="skill-bar">
+                            <p><strong>Passing under Pressure:</strong></p>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar"
+                                    style="width: ${player['Passing Under Pressure']}%;"
+                                    aria-valuenow="${player['Passing Under Pressure']}" aria-valuemin="0"
+                                    aria-valuemax="100">${player['Passing Under Pressure']}%</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="skill-bar">
-            <p><strong>Ball Control:</strong></p>
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" style="width: ${player['Ball Control']}%;" aria-valuenow="${player['Ball Control']}" aria-valuemin="0" aria-valuemax="100">${player['Ball Control']}%</div>
-            </div>
-        </div>
-        <div class="skill-bar">
-            <p><strong>Passing under Pressure:</strong></p>
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" style="width: ${player['Passing Under Pressure']}%;" aria-valuenow="${player['Passing Under Pressure']}" aria-valuemin="0" aria-valuemax="100">${player['Passing Under Pressure']}%</div>
-            </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
     </div>
 </div>
-`;
-    return playerCardHtml;
+  `
 }
